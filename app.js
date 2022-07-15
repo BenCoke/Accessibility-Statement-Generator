f1f11aecc87960a19485b88a7aaa0202b8b5ff5b
BenCoke <bencoke49@gmail.com>
2022-07-15 16:31:45 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,12 +2,41 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  console.log(`App listening on port ${port}`);
 });
 
-app.use(express.static('pages'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/background', (req, res) => {
+  res.render('background');
+});
+
+app.get('/improving-accessibility', (req, res) => {
+  res.render('improving-accessibility');
+});
+
+app.get('/outside-scope', (req, res) => {
+  res.render('outside-scope');
+});
+
+app.get('/non-accessible-content', (req, res) => {
+  res.render('non-accessible-content');
+});
+
+app.get('/disproportionate-burden', (req, res) => {
+  res.render('disproportionate-burden');
+});
+
+app.get('/compliance', (req, res) => {
+  res.render('compliance');
+});
+
+app.use('/public', express.static('public'));
+app.use('/dist', express.static('node_modules/@scottish-government/pattern-library/dist'));
+app.use('/src', express.static('node_modules/@scottish-government/pattern-library/src'));
+app.use('/library', express.static('node_modules/@scottish-government/pattern-library'));
